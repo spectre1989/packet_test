143eb802130be41d54ce7fd4d640e4fa7720dad2
Joe Best-Rotheray <joebestrotheray@gmail.com>
2017-10-02 19:32:11 +0100

    little bit of server work, will now refactor

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,32 @@
 
 
+static float time_since_s(LARGE_INTEGER* t, LARGE_INTEGER* freq)
+{
+	LARGE_INTEGER now;
+	QueryPerformanceCounter(&now);
+
+	LARGE_INTEGER time_since;
+	time_since.QuadPart = now.QuadPart - t->QuadPart;
+
+	return (float)time_since.QuadPart / (float)freq->QuadPart;
+}
+
+
 int main()
 {
+	WSADATA wsa_data;
+	int result = WSAStartup(0x202, &wsa_data);
+	assert(result == 0);
+
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	u_long enabled = 1;
+	result = ioctlsocket(sock, FIONBIO, &enabled);
+	assert(result != SOCKET_ERROR);
+
+	LARGE_INTEGER clock_frequency;
+	QueryPerformanceFrequency(&clock_frequency);
+	
     return 0;
 }
 
